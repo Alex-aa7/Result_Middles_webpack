973c1418597aedc4a6e35cbd14d40e460621dc0f
Ковалёв Тарас Викторович (4067694) <kovalevtv@vtb.ru>
2023-02-12 14:44:31 +0300

    make change bg img

---
+++ 00-webpack-start/src/constants.js
@@ -0,0 +1,20 @@
+export const soundsSettings = [
+    {
+        code: 'summer',
+        iconImg:'',
+        bgImg: '/assets/bg/summer-bg.jpg',
+        soundSrc: "./assets/sounds/summer.mp3"
+    },
+    {
+        code: 'rainy',
+        iconImg:'',
+        bgImg: '/assets/bg/rainy-bg.jpg',
+        soundSrc: "./assets/sounds/rain.mp3"
+    },
+    {
+        code: 'winter',
+        iconImg:'',
+        bgImg: '/assets/bg/winter-bg.jpg',
+        soundSrc: "./assets/sounds/winter.mp3"
+    },
+]
--- 00-webpack-start/src/index.js
+++ 00-webpack-start/src/index.js
@@ -1,55 +1,87 @@
 import './index.scss';
+import {soundsSettings} from "./constants";
 // const btn = document.querySelector(".button");
-const seasons = document.querySelector(".seasons");
-let volume = document.getElementById('volume-slider');
-
-volume.addEventListener("change", function(e) {
-      audio.forEach(function(elem,i) { audio[i].volume = e.currentTarget.value / 100})
-    console.log(audio[0].volume)
-})
-
-const data = [
-    { id: 0, img: "./img/summer-bg.jpg", sound: {src: "/assets/sounds/summer.mp3"} },
-  { id: 1, img: "./img/rainy-bg.jpg", sound: {src: "/assets/sounds/rain.mp3"} },
-  { id: 2, img: "./img/winter-bg.jpg", sound: {src: "/assets/sounds/winter.mp3"} },
-];
-
-const audio = [];
+const $seasons = document.querySelector("#seasons");
+const $seasonContainer = document.querySelector("#seasonContainer");
+const volume = document.querySelector('#volume-slider');
+const audios = [];
+let currentSeason = 0;
 let lastSeason = -1;
 
+volume.addEventListener("change", (e) => {
+    audios.forEach((elem, i) => {
+        audios[i].volume = e.currentTarget.value / 100
+    })
+    console.log('audio:', audios);
+    console.log(audios[0].volume)
+})
+
+
 seasons.addEventListener("click", function (e) {
-  selectSeason(e.target.id);
+    selectSeason(e.target.id);
 });
 
 function selectSeason(season) {
-  audio[season] ? '': (audio[season] = new Audio(data[season].sound));
+    audios[season] ? '' : (audios[season] = new Audio(data[season].sound));
 
-  if (lastSeason === season) {
-    togglePlay(season);
-  } else {
-    if (lastSeason === -1) {
-      togglePlay(season);
+    if (lastSeason === season) {
+        togglePlay(season);
     } else {
-      pause(lastSeason);
-      play(season);
+        if (lastSeason === -1) {
+            togglePlay(season);
+        } else {
+            pause(lastSeason);
+            play(season);
+        }
+        lastSeason = season;
     }
-    lastSeason = season;
-  }
 }
 
 function togglePlay(season) {
-  console.log(season);
-  return audio[season].paused ? audio[season].play() : audio[season].pause();
+    console.log(season);
+    return audios[season].paused ? audios[season].play() : audios[season].pause();
 }
+
 function pause(season) {
-  console.log("pause " + season);
-  return audio[season].pause();
+    console.log("pause " + season);
+    return audios[season].pause();
 }
+
 function play(season) {
-  console.log("play " + season);
-  return audio[season].play();
+    console.log("play " + season);
+    return audios[season].play();
 }
+
 function setVolume(volume) {
     console.log(volume);
-    return audio[season].volume=volume;
- }
+    return audios[season].volume = volume;
+}
+
+const createSoundItem = ({bgImg}, index) => {
+    const $soundItem = document.createElement('div');
+    $soundItem.classList.add('season');
+    $soundItem.style.backgroundImage = `url(${bgImg})`;
+    $soundItem.addEventListener('click', () => {
+        setCuttentItem(index)
+        updateCommonBg();
+    });
+
+    return $soundItem;
+}
+
+const setCuttentItem = (index) => {
+    currentSeason = index;
+}
+
+const updateCommonBg = () => {
+    $seasonContainer.style.backgroundImage = `url(${soundsSettings[currentSeason].bgImg})`
+}
+
+function init() {
+    soundsSettings.forEach((itemSettings, index) => {
+        $seasons.append(createSoundItem(itemSettings, index));
+    });
+    updateCommonBg();
+}
+
+init();
